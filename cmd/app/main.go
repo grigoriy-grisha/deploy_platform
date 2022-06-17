8aa4ffe88e74e10cc33e660c34b61a592ea0360a
Gavrilov Grigoriy <g.gavrilov@worksolutions.ru>
2022-06-17 09:05:29 +0300

    feat. Добавлен viper, порт вынесен в yml файл

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,14 +3,25 @@ package main
 import (
 	initServerApp "github.com/kybasas/cdcd_platform"
 	"github.com/kybasas/cdcd_platform/internal/controller"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
 	server := new(initServerApp.Server)
 
-	if err := server.Run("5000", controller.InitHandlers()); err != nil {
+	if err := initConfig(); err != nil {
 		log.Fatal(err.Error())
 	}
 
+	if err := server.Run(viper.GetString("port"), controller.InitHandlers()); err != nil {
+		log.Fatal(err.Error())
+	}
+
+}
+
+func initConfig() error {
+	viper.AddConfigPath("internal/config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
